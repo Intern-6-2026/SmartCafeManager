86cceb0e5d770dfe80671bf45c07417fd0040394
Tu23IT296 <tunt.23itvku.udn.vn>
2026-08-03 16:58:47 +0700

    ...

--- backend/src/main/java/com/codegym/backend/entity/TableOrder.java
+++ backend/src/main/java/com/codegym/backend/entity/TableOrder.java
@@ -1,11 +1,26 @@
 package com.codegym.backend.entity;
 
-import jakarta.persistence.*;
-import com.codegym.backend.enums.PaymentMethod;
-import com.codegym.backend.enums.StatusTableOrder;
-import lombok.*;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
+
+import com.codegym.backend.enums.PaymentMethod;
+import com.codegym.backend.enums.StatusTableOrder;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @jakarta.persistence.Table(name = "table_order")
